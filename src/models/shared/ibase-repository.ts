e8951f3ffcb331f519fb8eeb4f5eb9f1054fcd6b
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-04-01 15:55:48 +0530

    Update ibase-repository.ts

--- src/models/shared/ibase-repository.ts
+++ src/models/shared/ibase-repository.ts
@@ -3,7 +3,7 @@ export interface IBaseRepository<EntityType> {
     update(_id: string, model: EntityType): Promise<void>;
     delete(_id: string): Promise<{ n: number }>;
 
-    find(sort: string, query: any, limit: number = 250, skip: number = 0): Promise<EntityType[]>;
+    find(sort: string, query: any, limit?: number, skip?: number): Promise<EntityType[]>;
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     findOne<T>(query: any): Promise<EntityType>;
     count(query: any): Promise<number>;

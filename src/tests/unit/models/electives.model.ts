0e23f03816e23c49a4e64a3a81b12385f96aa484
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-04-14 15:13:41 +0530

    Added guaranteed batches for setupMockElectives, send responses to form

--- src/tests/unit/models/electives.model.ts
+++ src/tests/unit/models/electives.model.ts
@@ -3,14 +3,60 @@ import { getMockBatches } from './user.model';
 import { Container } from 'typescript-ioc';
 import faker from 'faker';
 import { IUserModel } from '../../../models/mongo/user-repository';
+import { BatchRepository } from '../../../models/mongo/batch-repository';
+import { FormsService } from '../../../routes/forms/service';
+import { ResponseService } from '../../../routes/response/service';
 
 export async function setupMockElectives(teachers: IUserModel[]): Promise<IElectiveModel[]> {
     const electiveRepository = Container.get(ElectiveRepository);
     const electives: IElectiveModel[] = [];
-    const teacherIds = teachers.map((e) => e.id);
-    for (let i = 0; i < 5; i++) {
+    const batches = await Container.get(BatchRepository).find('', '', undefined, 0);
+    const derivedBatches: string[][] = [];
+    for (let i = 0 ; i < 5; i++) {
         // @ts-ignore
-        electives.push(await electiveRepository.create(getMockElective(teacherIds)));
+        derivedBatches.push([
+            ...new Set(
+                faker.datatype
+                .array(faker.datatype.number({ min: 1, max: 5 }))
+                .map(() => faker.random.arrayElement(batches))
+                .map(e => e.id)
+            )
+        ]);
+        if (i === 4) {
+            let list: string[] = [];
+            for (const v of derivedBatches) {
+                list.push(...v);
+            }
+            list = [...new Set(list)];
+            if (list.length !== 5) {
+                // @ts-ignore
+                const missing = batches.filter(e => list.indexOf(e.id) === -1);
+                // @ts-ignore
+                derivedBatches[i].push(...missing.map(e => e.id));
+            }
+        }
+        electives.push(await electiveRepository.create({
+            name: faker.lorem.words(3),
+            description: faker.lorem.words(15),
+            courseCode: faker.datatype.uuid(),
+            version: faker.datatype.number({ min: 1 }),
+            strength: faker.datatype.number({ min: 1, max: 100 }),
+            // @ts-ignore
+            attributes: faker.datatype
+            .array(faker.datatype.number({ min: 1, max: 5 }))
+            .map(() => ({key: faker.lorem.word(), value: faker.lorem.word()})),
+            // @ts-ignore
+            batches: derivedBatches[i],
+            // @ts-ignore
+            teachers: [
+                ...new Set(
+                    faker.datatype
+                    .array(faker.datatype.number({ min: 1, max: 5 }))
+                    .map(() => faker.random.arrayElement(teachers))
+                    .map(e => e.id)
+                )
+            ]
+        }));
     }
     return electives;
 }
@@ -54,4 +100,21 @@ export function getMockElective(teachers: string[]): IElectiveModel {
             )
         ].join(',')
     };
+}
+
+export async function sendResponsesToForms(users: IUserModel[]): Promise<void> {
+    const formsService = Container.get(FormsService);
+    const responseService = Container.get(ResponseService);
+    for (const v of users) {
+        // @ts-ignore
+        const form = (await formsService.getActiveForms(v.id,'student'))[0];
+        const chosen = form.electives.slice(0, form.num).map(e => e.id);
+        await responseService.respondToForm({
+            // @ts-ignore
+            id: form.id,
+            // @ts-ignore
+            electives: faker.helpers.shuffle(chosen)
+        }, { id: v.id });
+    }
+    return;
 }

2cfa2e4ab5776695e796d3f865679599654aa0ca
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-04-26 19:38:34 +0530

    Revert "Update controller.ts"
    This reverts commit e837f8b10d1a316c9452f089352f9510d113f0ec.

--- src/routes/forms/controller.ts
+++ src/routes/forms/controller.ts
@@ -27,7 +27,6 @@ export interface UpdateFormOptions {
     shouldSelect?: number;
     selectAllAtForm?: boolean;
     electives?: string[];
-    active?:boolean;
 }
 
 export interface GenerateListResponse {
